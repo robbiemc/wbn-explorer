f086bb1787be73c963199ff024f46de54cc25193
Robbie McElrath <robbiemcelrath@gmail.com>
2023-10-28 22:46:01 -0700

    Hook up new components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,53 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { computed, ref, watch } from 'vue';
+
+import { BundleReader, WebBundle } from './BundleReader';
+import BundleChooser from './components/BundleChooser.vue';
+import Tree from './components/Tree.vue';
+
+const bundleFile = ref<File | null>(null);
+const bundle = ref<WebBundle | null>(null);
+
+watch(bundleFile, async () => {
+  if (bundleFile.value === null) {
+    bundle.value = null;
+    return;
+  }
+  const reader = new BundleReader(bundleFile.value);
+  const readResult = await reader.read();
+  if (typeof readResult === 'string') {
+    // TODO: handle errors
+    bundle.value = null;
+    return;
+  }
+  bundle.value = readResult;
+});
+
+const nodes = [{
+  id: 'id1',
+  name: 'name1',
+  children: [
+    { id: 'id2', name: 'name2' },
+    { id: 'id3', name: 'name3' },
+  ]
+}, {
+  id: 'id4',
+  name: 'name4'
+}];
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto">
+    <div class="p-4 text-2xl font-bold">Web Bundle Explorer</div>
+    <div class="p-4 flex flex-row flex-wrap">
+      <aside class="w-full sm:w-1/3 md:w-1/4 sticky">
+        <Tree v-if="bundle !== null" :nodes="nodes"></Tree>
+      </aside>
+      <main class="w-full sm:w-2/3 md:w-3/4">
+        <BundleChooser v-model:bundle-file="bundleFile"></BundleChooser>
+      </main>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
