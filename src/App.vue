7b9d50840c60c8f23a9d8751a16cf6f99d83009e
Robbie McElrath <robbiemcelrath@gmail.com>
2023-11-12 14:45:33 -0800

    Hook up side panel file opening

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { computed, ref, watch } from 'vue';
 
 import { BundleReader, WebBundle } from './BundleReader';
-import BundleChooser from './components/BundleChooser.vue';
 import DropTarget from './components/DropTarget.vue';
 import ResourceInfo from './components/ResourceInfo.vue';
 import { TreeNode } from './components/Tree.vue';
@@ -16,6 +15,21 @@ const expandTree = ref<boolean>(true);
 const bundleFile = ref<File | undefined>();
 const bundle = ref<WebBundle | undefined>();
 const selected = ref<string | undefined>();
+
+function onFileSelected(event: Event) {
+  const input = event.target as HTMLInputElement;
+  const files = input.files as FileList;
+  // TODO: handle multiple files
+  bundleFile.value = files[0];
+}
+
+function onDrop(files: File[]) {
+  if (files.length != 1) {
+    // TODO: handle multiple files
+    return;
+  }
+  bundleFile.value = files[0];
+}
 
 watch(bundleFile, async () => {
   selected.value = undefined;
@@ -101,7 +115,7 @@ function unmapTreeNode(mapped: MappedTreeNode): TreeNode[] {
         <div
           class="p-1 flex gap-1 items-stretch border-b dark:border-slate-600"
         >
-          <button class="flex px-1 rounded hover:bg-slate-500" @click="">
+          <label class="flex px-1 rounded hover:bg-slate-500 cursor-pointer">
             <svg
               class="inline w-4 text-gray-800 dark:text-white"
               aria-hidden="true"
@@ -117,9 +131,17 @@ function unmapTreeNode(mapped: MappedTreeNode): TreeNode[] {
                 d="M2.539 17h12.476l4-9H5m-2.461 9a1 1 0 0 1-.914-1.406L5 8m-2.461 9H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.443a1 1 0 0 1 .8.4l2.7 3.6H16a1 1 0 0 1 1 1v2H5"
               />
             </svg>
-          </button>
+            <input
+              class="hidden"
+              type="file"
+              accept=".wbn,.swbn"
+              @change="onFileSelected"
+            />
+          </label>
           <h2>
-            <b v-if="bundle !== undefined" class="text-l">{{ bundle.filename }}</b>
+            <b v-if="bundle !== undefined" class="text-l">{{
+              bundle.filename
+            }}</b>
             <i v-else>Select a .wbn/.swbn file</i>
           </h2>
           <span class="flex-grow"><!-- padding --></span>
@@ -169,6 +191,7 @@ function unmapTreeNode(mapped: MappedTreeNode): TreeNode[] {
         <DropTarget
           :file-types="bundleTypes"
           v-model:dragging="draggingOnTree"
+          @drop="onDrop"
           class="flex-grow overflow-y-auto border-dashed border-4"
           :class="{
             'border-transparent': !draggingOnPage,
@@ -195,10 +218,6 @@ function unmapTreeNode(mapped: MappedTreeNode): TreeNode[] {
             :bundle="bundle"
             :url="selected"
           ></ResourceInfo>
-          <BundleChooser
-            v-else
-            v-model:bundle-file="bundleFile"
-          ></BundleChooser>
         </div>
       </main>
     </div>
