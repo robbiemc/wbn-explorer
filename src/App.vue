6523c3f975808d74104dc11fb6e21a6e41d89a44
Robbie McElrath <robbiemcelrath@gmail.com>
2023-11-12 16:26:03 -0800

    Move side panel into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,122 +1,28 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
+import { ref } from 'vue';
 
-import { BundleReader, WebBundle } from './BundleReader';
-import { treeifyBundle } from './treeify-bundle';
+import { WebBundle } from './BundleReader';
 
 import DropTarget from './components/DropTarget.vue';
 import ResourceInfo from './components/ResourceInfo.vue';
-import SvgIcon from './components/SvgIcon.vue';
-import Tree from './components/Tree.vue';
-
-const bundleTypes = ['.wbn', '.swbn'];
+import SidePanel from './components/SidePanel.vue';
 
 const draggingOnPage = ref<boolean>(false);
-const draggingOnTree = ref<boolean>(false);
-const expandTree = ref<boolean>(true);
 
-const bundleFile = ref<File | undefined>();
 const bundle = ref<WebBundle | undefined>();
 const selected = ref<string | undefined>();
-
-function onFileSelected(event: Event) {
-  const input = event.target as HTMLInputElement;
-  const files = input.files as FileList;
-  // TODO: handle multiple files
-  bundleFile.value = files[0];
-}
-
-function onDrop(files: File[]) {
-  if (files.length != 1) {
-    // TODO: handle multiple files
-    return;
-  }
-  bundleFile.value = files[0];
-}
-
-watch(bundleFile, async () => {
-  selected.value = undefined;
-  if (bundleFile.value === undefined) {
-    bundle.value = undefined;
-    return;
-  }
-  const reader = new BundleReader(bundleFile.value);
-  const readResult = await reader.read();
-  if (typeof readResult === 'string') {
-    // TODO: handle errors
-    bundle.value = undefined;
-    return;
-  }
-  bundle.value = readResult;
-});
-
-const bundleTree = computed(() => {
-  if (bundle.value === undefined) {
-    return undefined;
-  }
-  return treeifyBundle(bundle.value);
-});
 </script>
 
 <template>
-  <DropTarget :file-types="bundleTypes" v-model:dragging="draggingOnPage">
+  <DropTarget :file-types="['.wbn', '.swbn']" v-model:dragging="draggingOnPage">
     <div class="container flex flex-row mx-auto">
       <aside class="w-full lg:w-1/3 xl:w-1/4 h-screen flex flex-col">
         <h1 class="py-2 text-2xl font-bold">Web Bundle Explorer</h1>
-        <div
-          class="p-1 flex gap-1 items-stretch border-b dark:border-slate-600"
-        >
-          <label class="flex px-1 rounded hover:bg-slate-500 cursor-pointer">
-            <SvgIcon class="inline w-4" type="folder_open"></SvgIcon>
-            <input
-              class="hidden"
-              type="file"
-              accept=".wbn,.swbn"
-              @change="onFileSelected"
-            />
-          </label>
-          <h2>
-            <b v-if="bundle !== undefined" class="text-l">{{
-              bundle.filename
-            }}</b>
-            <i v-else>Select a .wbn/.swbn file</i>
-          </h2>
-          <span class="flex-grow"><!-- padding --></span>
-          <button
-            v-if="bundle !== undefined"
-            class="flex px-1 rounded hover:bg-slate-500"
-            @click="expandTree = true"
-          >
-            <SvgIcon class="inline w-4" type="plus"></SvgIcon>
-          </button>
-          <button
-            v-if="bundle !== undefined"
-            class="flex px-1 rounded hover:bg-slate-500"
-            @click="expandTree = false"
-          >
-            <SvgIcon class="inline w-4" type="minus"></SvgIcon>
-          </button>
-        </div>
-        <DropTarget
-          :file-types="bundleTypes"
-          v-model:dragging="draggingOnTree"
-          @drop="onDrop"
-          class="flex-grow overflow-y-auto border-dashed border-4"
-          :class="{
-            'border-transparent': !draggingOnPage,
-            'border-slate-600': draggingOnPage,
-            'bg-slate-700': draggingOnPage && !draggingOnTree,
-            'bg-slate-600': draggingOnTree,
-          }"
-        >
-          <Tree
-            v-if="bundleTree !== undefined"
-            :nodes="bundleTree"
-            :selected="selected"
-            :expanded="expandTree"
-            @node-selected="(id) => (selected = id)"
-          ></Tree>
-        </DropTarget>
+        <SidePanel
+          :dragging-on-page="draggingOnPage"
+          v-model:bundle="bundle"
+          v-model:selected="selected"
+        ></SidePanel>
       </aside>
       <main class="w-full lg:w-2/3 xl:w-3/4">
         <h1 class="py-2 text-2xl font-bold"><!-- Spacing -->&nbsp;</h1>
---
+++ src/components/SidePanel.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+
+import { BundleReader, WebBundle } from '../BundleReader';
+import { treeifyBundle } from '../treeify-bundle';
+
+import DropTarget from './DropTarget.vue';
+import SvgIcon from './SvgIcon.vue';
+import Tree from './Tree.vue';
+
+const props = defineProps<{
+  bundle?: WebBundle;
+  selected?: string;
+  draggingOnPage: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: 'update:bundle', bundle?: WebBundle): void;
+  (e: 'update:selected', id?: string): void;
+}>();
+
+const bundleTypes = ['.wbn', '.swbn'];
+
+const draggingOnTree = ref<boolean>(false);
+const expandTree = ref<boolean>(true);
+
+const bundleFile = ref<File | undefined>();
+
+function onFileSelected(event: Event) {
+  const input = event.target as HTMLInputElement;
+  const files = input.files as FileList;
+  // TODO: handle multiple files
+  bundleFile.value = files[0];
+}
+
+function onDrop(files: File[]) {
+  if (files.length != 1) {
+    // TODO: handle multiple files
+    return;
+  }
+  bundleFile.value = files[0];
+}
+
+watch(bundleFile, async () => {
+  emit('update:selected', undefined);
+  if (bundleFile.value === undefined) {
+    emit('update:bundle', undefined);
+    return;
+  }
+  const reader = new BundleReader(bundleFile.value);
+  const readResult = await reader.read();
+  if (typeof readResult === 'string') {
+    // TODO: handle errors
+    emit('update:bundle', undefined);
+    return;
+  }
+  emit('update:bundle', readResult);
+});
+
+const bundleTree = computed(() => {
+  if (props.bundle === undefined) {
+    return undefined;
+  }
+  return treeifyBundle(props.bundle);
+});
+</script>
+
+<template>
+  <div class="p-1 flex gap-1 items-stretch border-b dark:border-slate-600">
+    <label class="flex px-1 rounded hover:bg-slate-500 cursor-pointer">
+      <SvgIcon class="inline w-4" type="folder_open"></SvgIcon>
+      <input
+        class="hidden"
+        type="file"
+        accept=".wbn,.swbn"
+        @change="onFileSelected"
+      />
+    </label>
+    <h2>
+      <b v-if="bundle !== undefined" class="text-l">{{ bundle.filename }}</b>
+      <i v-else>Select a .wbn/.swbn file</i>
+    </h2>
+    <span class="flex-grow"><!-- padding --></span>
+    <button
+      v-if="bundle !== undefined"
+      class="flex px-1 rounded hover:bg-slate-500"
+      @click="expandTree = true"
+    >
+      <SvgIcon class="inline w-4" type="plus"></SvgIcon>
+    </button>
+    <button
+      v-if="bundle !== undefined"
+      class="flex px-1 rounded hover:bg-slate-500"
+      @click="expandTree = false"
+    >
+      <SvgIcon class="inline w-4" type="minus"></SvgIcon>
+    </button>
+  </div>
+  <DropTarget
+    :file-types="bundleTypes"
+    v-model:dragging="draggingOnTree"
+    @drop="onDrop"
+    class="flex-grow overflow-y-auto border-dashed border-4"
+    :class="{
+      'border-transparent': !draggingOnPage,
+      'border-slate-600': draggingOnPage,
+      'bg-slate-700': draggingOnPage && !draggingOnTree,
+      'bg-slate-600': draggingOnTree,
+    }"
+  >
+    <Tree
+      v-if="bundleTree !== undefined"
+      :nodes="bundleTree"
+      :selected="selected"
+      :expanded="expandTree"
+      @node-selected="(id) => emit('update:selected', id)"
+    ></Tree>
+  </DropTarget>
+</template>
