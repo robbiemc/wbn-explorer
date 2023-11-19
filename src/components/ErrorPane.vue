69b9546931bc1647482ab919f2b40f7723d495ae
Robbie McElrath <robbiemcelrath@gmail.com>
2023-11-19 11:52:07 -0800

    Add error messages

--- src/components/ErrorPane.vue
+++ src/components/ErrorPane.vue
@@ -9,11 +9,15 @@ defineProps<{
 function stringifyError(error: Error) {
   switch (error) {
     case 'BAD_MAGIC':
+      return 'Invalid bundle: expected magic bytes \'🖋📦\' or \'🌐📦\'';
     case 'SIG_INVALID':
+      return 'The integrity block has an unrecognized structure.';
     case 'SIG_UNKNOWN_VERSION':
+      return 'The integrity block has an unknown version.';
     case 'SIG_UNSUPPORTED_KEY_TYPE':
+      return 'Unsupported public key type in integrity block. Only ed25519 is supported';
     case 'TOO_MANY_FILES':
-      return error;
+      return 'Too many files were selected.';
   }
 }
 </script>
